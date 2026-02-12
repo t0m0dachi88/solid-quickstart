2151f3d09969b8248c43a6f98a544f83f8b61499
Tomodachi <86731445+t0m0dachi88@users.noreply.github.com>
2026-02-12 07:07:56 +0000

    added data model;

---
+++ models/to-dos/sub_todos.models.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const subTodosSchema=new mongoose.Schema({
+    content:{
+           type:String,
+           required:true
+       },
+       isComplete:{
+           type:Boolean,
+           default:false
+       },
+       createdBy:{
+           type:mongoose.Schema.Types.ObjectId,
+           ref:"User"
+       }
+
+},{timestamps:true})
+
+export const subTodos=mongoose.model("SubTodo",subTodosSchema)
